b99db72608729c136814a49726605431d9a3d80e
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-09-12 17:23:10 +0300

    My Spring Fundamentals Project

---
+++ CarsBg/src/main/java/com/softuni/CarsBg/CarsBgApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.CarsBg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsBgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsBgApplication.class, args);
+	}
+
+}
---
+++ CarsBg/src/test/java/com/softuni/CarsBg/CarsBgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.CarsBg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsBgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
