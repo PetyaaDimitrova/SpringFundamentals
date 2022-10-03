695416970964ed10ee1e2a1d550780813f6948eb
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-03 17:04:22 +0300

    Add files via upload

--- CarsBg/src/main/java/com/softuni/CarsBg/web/UserRegisterController.java
+++ CarsBg/src/main/java/com/softuni/CarsBg/web/UserRegisterController.java
@@ -1,15 +1,20 @@
 package com.softuni.CarsBg.web;
 
-import com.softuni.CarsBg.model.dto.UserLoginDTO;
 import com.softuni.CarsBg.model.dto.UserRegisterDTO;
 import com.softuni.CarsBg.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/users")
 public class UserRegisterController {
     private UserService userService;
 
@@ -17,14 +22,27 @@ public class UserRegisterController {
         this.userService = userService;
     }
 
+    @ModelAttribute("userModel")
+    public UserRegisterDTO initUserModel() {
+        return new UserRegisterDTO();
+    }
 
-    @GetMapping("/users/register")
+    @GetMapping("/register")
     public String register(){
         return "auth-register";
     }
 
-    @PostMapping("/users/register")
-    public String register(@Valid UserRegisterDTO userRegisterDTO) {
+    @PostMapping("/register")
+    public String register(@Valid UserRegisterDTO userRegisterDTO,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("userModel", userRegisterDTO);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userModel",
+                    bindingResult);
+
+            return "redirect:/users/register";
+        }
         userService.registerAndLogin(userRegisterDTO);
         return "redirect:/";
     }
